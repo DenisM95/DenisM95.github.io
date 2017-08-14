82e72ac07d138934edcecbcaa4ec89c7c3831360
DenisM95 <denismarchenko_95@mail.ru>
2017-08-14 18:44:11 +1000

    v1.2

---
+++ js.js
@@ -0,0 +1,6 @@
+function move() {
+    document.getElementById('triangle').background='red'
+}
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
---
+++ js/js.js
@@ -0,0 +1,15 @@
+function move_1() {
+    document.getElementById('triangle').style.left = 'calc(0% - 10px)';
+};
+function move_2() {
+    document.getElementById('triangle').style.left = 'calc(20% - 10px)';
+};
+function move_3() {
+    document.getElementById('triangle').style.left = 'calc(50% - 10px)';
+};
+function move_4() {
+    document.getElementById('triangle').style.left = 'calc(100% - 10px)';
+};
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
